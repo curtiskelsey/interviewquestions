832af99d1fb7e46f0a585beac5ad88a01a19a408
Curtis Kelsey <curtis@lodgable.com>
2020-02-25 12:11:25 -0600

    Added a rental service to simplify calculating rental totals and a rental service trait to simplify the consumption of the service

---
+++ php_video_rental_statement/src/Service/RentalService.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace AxisCare\Service;
+
+use AxisCare\Rental;
+
+/**
+ * Class RentalService
+ * @package AxisCare\Service
+ */
+class RentalService
+{
+    /**
+     * Given a rental, returns the total cost of the rental
+     *
+     * @param Rental $rental
+     * @return float
+     */
+    public function getTotal(Rental $rental): float
+    {
+        $movie = $rental->getMovie();
+        $priceCode = $movie->getPriceCode();
+
+        $amount = $priceCode->getFlatRate();
+
+        $chargeableDays = $rental->getDaysRented() - $priceCode->getDaysRentedThreshold();
+
+        if ($chargeableDays < 0) {
+            $chargeableDays = 0;
+        }
+
+        $amount += ($chargeableDays * $priceCode->getPriceMultiplier());
+
+        return $amount;
+    }
+}
---
+++ php_video_rental_statement/src/Service/RentalServiceAwareTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AxisCare\Service;
+
+/**
+ * Class RentalServiceAwareTrait
+ * @package AxisCare\Service
+ */
+trait RentalServiceAwareTrait
+{
+    /** @var  RentalService */
+    protected $rentalService;
+
+    /**
+     * @return RentalService
+     */
+    public function getRentalService(): RentalService
+    {
+        return $this->rentalService;
+    }
+
+    /**
+     * @param RentalService $rentalService
+     * @return $this
+     */
+    public function setRentalService(RentalService $rentalService): self
+    {
+        $this->rentalService = $rentalService;
+        return $this;
+    }
+}
