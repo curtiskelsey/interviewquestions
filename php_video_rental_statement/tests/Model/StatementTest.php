ba558d81b4e6dc73695e17f024cc72f13d41f770
Curtis Kelsey <curtis@lodgable.com>
2020-02-25 12:39:21 -0600

    Added some statement model tests

---
+++ php_video_rental_statement/tests/Model/StatementTest.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace AxisCareTest\Model;
+
+use AxisCare\Model\Customer;
+use AxisCare\Model\Statement;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class StatementTest
+ * @package AxisCareTest\Model
+ */
+class StatementTest extends TestCase
+{
+    public function testAddToTotal()
+    {
+        $statement = new Statement(
+            new Customer('test')
+        );
+
+        $this->assertEquals(0, $statement->getTotal());
+
+        $statement->addToTotal(3);
+
+        $this->assertEquals(3, $statement->getTotal());
+    }
+
+    public function testAddFrequentRenterPoints()
+    {
+        $statement = new Statement(
+            new Customer('test')
+        );
+
+        $this->assertEquals(0, $statement->getFrequentRenterPoints());
+
+        $statement->addFrequentRenterPoints(3);
+
+        $this->assertEquals(3, $statement->getFrequentRenterPoints());
+    }
+
+    public function testAddLineItem()
+    {
+        $statement = new Statement(
+            new Customer('test')
+        );
+
+        $this->assertCount(0, $statement->getLineItems());
+
+        $statement->addLineItem('test', 1);
+
+        $this->assertCount(1, $statement->getLineItems());
+    }
+}
